504d6e63807f0019a8c23ef13ccafe28e1dd21fc
MullenratteYRC <max@yew-rune-creations.com>
2024-11-01 18:34:33 +0100

    Initial commit

---
+++ Source/Agricana/Agricana.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Agricana.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Agricana, "Agricana" );
